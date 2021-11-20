e62a00d484313723dc3087706c0aa84a96ba80a7
Gabriel Mihalache <mihalache.gabriel16@gmail.com>
2021-11-20 18:45:07 +0200

    mici modificari

--- src/Main.java
+++ src/Main.java
@@ -32,15 +32,56 @@ public class Main {
         int[] array8main = {1, 5, 9, 21, 4, 2, 10};
         int nr = 4;
         op.array2print(op.array8(array8main, nr));
+        System.out.println("-----------");
 
         //Cerinta 9 array
         int[] arraysort = {5, 10,1, 3, 20, 9, 100};
         System.out.println(op.array9(arraysort));
+        System.out.println("-----------");
 
         //Cerinta 10 array
         int[] arraycerinta10 = {5, 20,1, 3, 20, 493, 100};
         int[] arraygol = new int[arraycerinta10.length];
         op.array10(arraycerinta10, arraygol);
         op.array2print(arraygol);
+
+
+        //Tema ARRAYS OPTIONALA
+        //Cerinta 1 optional
+        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        op.opt1(my_array, 5 ,1999);
+        op.array2print(my_array);
+        System.out.println("-----------");
+
+        //Cerinta 2 optionala
+        int[] array2opt = {8, 302, 21, 523, 2, 11};
+        op.opt2(array2opt);
+        System.out.println("-----------");
+
+        //Cerinta 3 optionala
+        int[] array3opt = {2, 5, 7, 1, 3, 10};
+        op.opt3(array3opt);
+        op.array2print(array3opt);
+        System.out.println("-----------");
+
+        //Cerinta 4 optionala
+        int[] array4opt = {2, 6, 7, 6, 3, 7, 3, 10, 7};
+        op.array2print(op.optDuplicate(array4opt));
+        System.out.println("-----------");
+
+        //Cerinta 5 optionala
+        int[] array01 = {2, 6, 3, 5, 10};
+        int[] array02 = {2, 9, 10, 1, 100, 99};
+        int m = array01.length;
+        int n = array02.length;
+        op.opt5(array01, array02, m, n);
+        System.out.println("\n-----------");
+
+        //Cerinta 6 optionala
+        int[] opt6array = {2, 6, 3, 5, 11};
+        op.array2print(op.array6opt(opt6array));
+
+
+
     }
 }
